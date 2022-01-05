502dc9a63e73746e8bf697d33fd33e7273c88c67
tedbeast <tedbalashov@gmail.com>
2022-01-05 12:14:48 -0500

    comments

--- javascript/hellojs.js
+++ javascript/hellojs.js
@@ -16,8 +16,8 @@
 console.log("Hi JS!"); // this line is equivalent to System.out.println(); in Java
 console.log('hi js!!');
 console.log(`Hi JS!!`); //template literal: the string is represented exactly as-is
-
 /*
+variable types:
 * string
 * number
 * boolean
@@ -28,24 +28,24 @@ console.log(`Hi JS!!`); //template literal: the string is represented exactly as
 */
 
 // semicolons are not necessary * 
-
+// creating vars:
 var a_name = "a name";
 var number = 4;
 var array = [0, 1, 2];
 var boolean = false;
-
+// variables can be anything:
 a_name = 4;
 a_name = [0, 1, true];
-
+// creating objects:
 a_name = {
     first_name : "jackie",
     last_name : "crosswhite"
 }
-
+// one way to create functions:
 a_name = function(){
     console.log("what???");
 }
-
+// calling functions:
 a_name();
 
 a_name="";
@@ -64,21 +64,70 @@ if(number){
     console.log("who???");
 }
 
+
 var a = 5;
 var b = 0;
-
-function pythagoreanTheory(sidex, sidey){
+// creating functions:
+function pythagoreanTheorem(sidex, sidey){
     var result = Math.sqrt(sidex*sidex+sidey*sidey);
     return result;
 }
+//calling functions:
+var sidez = pythagoreanTheorem(4,5);
+console.log(sidez);
+// control flow (if statements)
 if(a>b){
     console.log("do something");
 }else{
     console.log("something else");
 }
-
+// == vs ===
 var c = 5;
 var d = "5";
+// true
+e = 5;
 if(c==d){
     console.log("equal");
+    var e = 2;
 }
+// false
+if(c===d){
+    console.log("equal");
+}
+// nested functions
+function parent(){
+    function child(){
+        
+    }
+}
+// control flow
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+c = false;
+// while
+while(c){
+console.log(c);
+}
+// do while
+do{
+    console.log(c);
+}while (c);
+
+/*
+hoisting: javascript pretends that all functions and variables are declared at the start of the file
+
+scopes:
+
+global: can be seen globally by the application
+function: can be seen within the function
+block: can be seen within a block (for loop,  if statement, case, trycatch)
+lexical: functions created in a function can have access to the variables in the parent function
+
+
+variable types:
+var - basic, global/function scope variable that can be anything
+ES6:
+let - all four types of scope
+const - variable that can never be changed
+*/
