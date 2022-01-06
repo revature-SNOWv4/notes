aa8604780ee1f724328e3622913d8353f462598e
tedbeast <tedbalashov@gmail.com>
2022-01-06 14:29:12 -0500

    pokedex json

--- javascript/pokedex.js
+++ javascript/pokedex.js
@@ -1,96 +1,46 @@
-/*
-    FILLED IN FOR USE WITH POKEAPI
-*/
-// Endpoint you are sending a GET request to
-var apiURL = 'https://pokeapi.co/api/v2/pokemon/';
-
+var apiURL = 'http://pokeapi.co/api/v2/pokemon/'
 document.getElementById('getData').onclick = getData;
-
-function getData() {
-    // If using input for identifiers, etc.
-    // For example, if using PokeAPI, this may be the Pokemon's ID.
+function getData (){
+    // AJAX
+    // Autononmous Javascript and XML
     var userInput = document.getElementById('dataInput').value;
 
-    // 4 steps to making an AJAX call
-    // STEP 1: Create an XML Http Request object
+    // 4 steps
+    // step 1: create an xml http request object
+
+    // http: hypter text tranfer protocol
+
+    // REST : REPRESENTATIONAL STATE TRANSFER
+    // GET, PUT, POST, DELETE, etc
+
     var xhttp = new XMLHttpRequest();
 
-    // STEP 2: Set a callback function for the readystatechange event
+    //step 2: set a callback function for the readystatechangeevent
+
     xhttp.onreadystatechange = receiveData;
 
-    // STEP 3: Open the request
+    // step 3: open the request
+
     xhttp.open('GET', apiURL + '' + userInput);
 
-    // STEP 4: Send the request
+    // step 4: send the request
+
     xhttp.send();
 
-    // This needs to be an inner function so that it has closure to xhttp.
-    function receiveData() {
+    function receiveData(){
         /*
-            Different ready states of an XMLHttpRequest object
-            0: UNSENT
-            1: OPENED
-            2: HEADERS RECEIVED
-            3: LOADING
-            4: DONE
+        0: unsent
+        1: opened
+        2: headers received
+        3: loading
+        4: done
         */
-        // Emptying out the div before inserting new data.
-        var dataSection = document.getElementById('data');
-        dataSection.innerHTML = '';
-        if (xhttp.readyState === 4) {
-            if (xhttp.status === 200) {
-                // Ready state is DONE, HTTP status code is "OK"
-                var response = xhttp.responseText;
-                response = JSON.parse(response);
-                populateData(response);
-            } else {
-                // Ready state is DONE but status code is not "OK"
-                dataSection.innerHTML = 'It Got Away!';
+       var dataSection = document.getElementById('data');
+       dataSection.innerHTML = '';
+        if(xhttp.readyState === 4){
+            if(xhttp.status === 200){
+                dataSection.innerHTML = xhttp.responseText;
             }
-        } else {
-            // Ready state is not DONE
-            /*
-                Can have some sort of "loading" action
-            */
         }
     }
-}
-
-function populateData(response) {
-    var dataSection = document.getElementById('data');
-    
-    var nameTag = document.createElement('h3');
-    nameTag.innerHTML =  capitalize(response.name);
-    var abilitiesArray = response.abilities;
-    var abilities = document.createElement('ul');
-    for (var ability of abilitiesArray)
-    {
-        var abilityLi = document.createElement('li');
-        abilityLi.innerHTML = capitalize(ability.ability.name);
-        abilities.appendChild(abilityLi);
-    }
-
-    dataSection.appendChild(nameTag);
-    dataSection.innerHTML += 'Abilities<br>';
-    dataSection.appendChild(abilities);
-
-    var spritesObject = response.sprites;
-    for (var sprite in spritesObject) {
-        if(spritesObject[sprite]) {
-            var spriteImg = document.createElement('img');
-            spriteImg.src = spritesObject[sprite];
-            dataSection.appendChild(spriteImg);
-        }
-    }
-}
-
-/*
-    The PokeAPI returns Pokemon's information as all lowercase.
-    This function is used to fix this when processing data.
-*/
-function capitalize(str) {
-    if (str)
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    else
-        return '';
 }
